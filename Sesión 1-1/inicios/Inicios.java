a2d21d496905481c58e9f7f44a5dc2b3793c2079
fleal <fleal@unitty.com>
2021-07-01 14:12:55 -0500

    Sesión julio 1 de 2021

--- Sesión 1-1/inicios/Inicios.java
+++ Sesión 1-1/inicios/Inicios.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class Inicios {
 
     public static void main(String[] args) {       
+        
+
+package origine2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+public class Origine2 {
+    
+    
+    public static void main(String[] args) {
+        
         // Entradas y salidas
         String nombre;
         
@@ -22,9 +39,9 @@ public class Inicios {
         System.out.print("Ingrese un número ramdon: ");
         float numero = in.nextFloat();
         System.out.println("Su número es " + numero);
-
-
-        // condicionales
+//
+//
+//        // condicionales
         boolean es_verdad = false;
         boolean segunda_pregunta = false;
         if( es_verdad ){
@@ -62,11 +79,11 @@ public class Inicios {
             default:
                 System.out.println("No es a ni b.");
         }
-        
-        
+
+
 
         // manejo de string
-        String nombre = "Freddy";
+        nombre = "Freddy";
         System.out.println( nombre.toLowerCase() );
         System.out.println( nombre.toUpperCase() );
         
@@ -76,14 +93,14 @@ public class Inicios {
         } else {
             System.out.println("Usuario NO autorizado");
         }
-        
-        
+
+
         
         // Ciclos FOR
 //        for item in range(10):
 //            print( item )
         
-        for(int i=0; i < 10; ++i ){
+        for(i=0; i < 10; ++i ){
 //            i++; - Este se ejecuta después de pasar la linea
 //            ++i; - Este se ejecuta antes de pasar la linea
             System.out.println(i);
@@ -112,10 +129,132 @@ public class Inicios {
             System.out.println(numero_2);
             numero_2++;
         } while ( numero_2 <= 20);
+
+
+//         array 
+        String array[] = {"Freddy", "Alejandro", "Leal"};
+        System.out.println( array[1] );
+        for(String x : array){
+            System.out.println( x );
+        }
+        
+        String[][] array2d = {
+            {"A", "B", "C"},
+            {"D", "E", "F"},
+            {"G", "H", "I"},
+        };
+        System.out.println( array2d[0][1] );
+
+
+        // ArrayList (Listas de python)
+        ArrayList<Integer> ar = new ArrayList<>();
+        
+        for(int cont=0; cont<100; cont++){
+            ar.add(cont);
+        }
+        System.out.println( ar.size() );
+        
+        for(int aux : ar){
+            System.out.println(aux);
+        }
         
         
+//         Maps ( los diccionarios de python)
+        Map<String, Object> diccionario = new HashMap();
+        
+        diccionario.put("nombre", "Freddy Leal");
+        diccionario.put("edad", 32);
+        
+        System.out.println( diccionario.get("nombre") );
+        
+        edad = (int) diccionario.get("edad");
+        System.out.println( edad );
+
+
+        // Operaciones Matematicas 
+        // + - * / %
+        numero = 5;
+        int x = (int) Math.pow(5, 2);
+        System.out.println(x);
+        int y = (int) Math.sqrt(x);
+        System.out.println(y);
+        
+        System.out.println( Math.PI );
+        System.out.println( Math.E );
+        
+
+        // operadores logicos
+        // == != < > <= >=   .equals  
+        // && ||
+        
+        if(true && true){
+            System.out.println("Entro");
+        } else {
+            System.out.println("Es falso");
+        }
+        
+        if(false || false){
+            System.out.println("Entro");
+        } else {
+            System.out.println("Es falso");
+        }
+        
+        // números aleatorios
+        Random generadorNumeros = new Random();
+        int aleatorio = 1 + generadorNumeros.nextInt(7);
+        System.out.println( aleatorio );
+        
+        
+        // fracciones de cadenas de string
+        String holaMundo = "Hola Mundo";
+        System.out.println( holaMundo.substring(3, 6) );
+        int index = holaMundo.indexOf('a');
+        letra = in.nextLine();
+        
+        if( holaMundo.contains( letra ) ){
+            System.out.println("Si esta la letra");
+        } else {
+            System.out.println("No esta la letra");
+        }
+        
+
+        // control de errores 
+        int a = 1;
+        int b = 0;
+        int c;
+        try{
+            c = a / b;
+        } catch( ArithmeticException e){
+            System.out.println("No se puede dividir por cero.");
+        }
+//        
+        try{
+            int d = Integer.parseInt("1234123");
+            System.out.println("El número es: " + d);
+            c = a / b;
+        } catch( NumberFormatException e){
+            System.out.println("Esto no es un número.");
+        } catch( ArithmeticException e){
+            System.out.println("No se puede dividir por cero.");
+        } finally {
+            System.out.println("Esto se ejecuta siempre");
+        }
+        
+        try{
+            float as = Float.parseFloat("asdf");
+        } catch( Exception e){
+            System.out.println("Atrapa todo si algo sale mal.");
+        }
+        
+        
+        // funciones
+        saludar("Freddy");
     }
     
+    
+    public static void saludar(String nombre){
+        System.out.println("Hola " + nombre);
+    }
+    
+    
 }
-
-
